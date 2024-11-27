85ed4d6b56f60e558e6ce338c9fbcb6a243bc295
Martí <mllorach6@gmail.com>
2024-11-27 21:26:24 +0100

    practica 3 hecha

--- Ejercicio1_marti.R
+++ Ejercicio1_marti.R
@@ -47,10 +47,43 @@ media = mean(simu) #sum(simu)/length(simu)
 media
 
 #¿Cuál es la varianza de los resultados de las simulaciones?
-f_simu = as.vector(fi)
-esperanza_simu = sum(x*f_simu)
-varianza_simu = sum( (x-esperanza_simu)^2 * f_simu )
-varianza_simu
+#f_simu = as.vector(fi)
+#esperanza_simu = sum(x*f_simu)
+#esperanza_simu
+#varianza_pob_simu = sum( (x-esperanza_simu)^2 * f_simu )
+#varianza_muestral_simu = varianza_simu * (length(simu)) / (length(simu) - 1)
 var(simu)
 
+#Ahora realiza 10000 simulaciones (usando la misma semilla) y compara resultados
+simu2 = sample(x, 10000, replace=TRUE, prob=f)
+media2 = mean(simu2)
+varianza2 = var(simu2)
+media2
+media
+varianza2
+varianza
 
+
+#Definamos una nueva variable aleatoria Y que representa el beneficio por las ventas.
+#  Y = 1.65 X + 34 ( 1.20 ) ( 5 − X ) − ( 1.20 ) ( 5 ) = 0.75 X − 1.5 
+#E(Y) = a*E(X) + b
+#Var(Y) = a^2+*E(X)
+espY = 0.75*esperanza - 1.5
+espY
+varY = 0.75^2*varianza
+varY
+
+#Realiza 10000 simulaciones del beneficio diario. (Utiliza como semilla el número 123). Representa gráficamente la frecuencia de los resultados obtenidos y compáralo con el diagrama de la función de probabilidad.
+set.seed(123)
+y = 0.75*x - 1.5
+simuY = sample(y, 10000, replace = TRUE, prob = f)
+fi = table(simuY)/length(simuY)
+xb = barplot(fi)
+lines(xb, f, type="h", lwd=3, col="red")
+points(xb, f, lwd=5, col="red")
+
+#¿Cuál es el valor de la media de los resultados de las simulaciones?
+mean(simuY)
+
+#¿Cuál es la varianza de los resultados de las simulaciones?
+var(simuY)
